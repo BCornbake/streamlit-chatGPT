3007af0ba4c25e00ebc81d421a685a4190b0fcf8
BCornbake <39626433+BCornbake@users.noreply.github.com>
2023-09-17 22:21:41 +0800

    update response error

--- src/main.py
+++ src/main.py
@@ -38,7 +38,7 @@ if prompt := st.chat_input("What is up?"):
             ],
             stream=True,
         ):
-            full_response += response.choices[0].delta.get("content", "")
+            full_response += response["choices"][0]["message"].get("content", "")
             message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
     st.session_state.messages.append({"role": "assistant", "content": full_response})
